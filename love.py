97a2ed32a6742de9ab6eab0a7c367a5d786afe9f
Andhikalol <106020230+Andhikalol@users.noreply.github.com>
2022-05-24 19:39:00 +0700

    Add files via upload

---
+++ love.py
@@ -0,0 +1,67 @@
+import os
+import random
+import sys
+import time
+os.system("clear")
+os.system("pkg install figlet")
+os.system("clear")
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.3)
+mengetik("hallo annisa, hari yang cerah ini saya andhika mau menyampaikan sesuatu hehehe")
+
+print ("")
+
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.3)
+mengetik("Kamu adalah pikiran terakhir di benakku sebelum aku tertidur dan pikiran pertama ketika aku bangun setiap pagi")
+
+print ("")
+
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.3)
+mengetik("Ada yang sabar menanti, menunggu yang tak terganti. Ada yang lelah menanti, akhirnya mencari pengganti dan aku mendapatkannya yaitu kamu")
+
+print ("")
+
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.3)
+mengetik("Semacam tidak memiliki namun takut kehilangan semacam tak punya status namun merasakan cemburu")
+
+print ("")
+
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.3)
+mengetik("Cintailah seseorang itu atas dasar siapa dia sekarang dan bukan siapa dia sebelumnya")
+
+print ("")
+
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.3)
+mengetik("Terimalah hatiku dan aku akan membangunkanmu kastil dengan cinta sebagai fondasinya")
+
+print ("")
+
+pilih = raw_input("tap enter untuk lanjut : ")
+if pilih =="":
+	os.system("clear")
+	print "tidak ada pilihan??, iya terimakasih"
+	os.system("python2 tembak")
+
